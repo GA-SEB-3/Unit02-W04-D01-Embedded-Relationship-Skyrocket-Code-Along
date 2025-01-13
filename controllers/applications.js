051afdae46ea6555c1ad27f5c8ce3ec802c1ac4b
omarakamal <omaraakamal@gmail.com>
2025-01-13 20:30:47 +0300

    init commit

--- controllers/applications.js
+++ controllers/applications.js
@@ -66,5 +66,47 @@ router.get('/:applicationId',async(req,res)=>{
   }
 })
 
+router.delete("/:applicationId",async (req,res)=>{
+  try{
+    const currentUser = await User.findById(req.session.user._id)
+
+    currentUser.applications.id(req.params.applicationId).deleteOne()
+
+    await currentUser.save()
+
+    res.redirect(`/users/${currentUser._id}/applications`)
+
+  }catch(error){
+    console.log(error)
+    res.redirect("/")
+  }
+})
+
+router.get('/:applicationId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/edit.ejs', {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.put("/:applicationId",async(req,res)=>{
+  const currentUser = await User.findById(req.session.user._id)
+
+  const application = currentUser.applications.id(req.params.applicationId)
+
+  application.set(req.body)
+
+  await currentUser.save()
+
+  res.redirect(`/users/${currentUser._id}/applications/${req.params.applicationId}`)
+
+})
+
 
 module.exports = router
